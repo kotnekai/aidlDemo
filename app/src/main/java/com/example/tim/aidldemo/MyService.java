d472c8e4bc5d93ccca7f1f276a91438aab259ace
TIM\Tim <tianhai.hu@meicloud.com>
2018-08-09 11:19:55 +0800

    first commit

---
+++ app/src/main/java/com/example/tim/aidldemo/MyService.java
@@ -0,0 +1,32 @@
+package com.example.tim.aidldemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return new MyBind();
+    }
+
+
+    class MyBind extends  IMyAidlInterface.Stub
+    {
+
+        @Override
+        public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
+
+        }
+
+        @Override
+        public String getName() throws RemoteException {
+            return "aidl_MyService_getName";
+        }
+    }
+}
